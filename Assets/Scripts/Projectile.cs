c58c0f63ce969763186e1c41e571a953b8eabff9
Ralt <Raltrwx@gmail.com>
2019-11-09 00:16:38 -0700

    Initial Test for Unity Upload to Git

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    // Awake() is used instead of Start(), and is called immediately when the object is created to initialize Rigidbody2d before calling Launch
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2D.AddForce(direction * force);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Projectile Collision with " + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 50.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
