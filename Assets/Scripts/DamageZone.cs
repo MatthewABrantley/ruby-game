325e8ba4f2def6b4c6b47087f0ebe0c20aec574d
Raltrwx <Raltrwx@gmail.com>
2019-11-09 12:01:59 -0700

    comments

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
+    // IF the 2d Collider "stays" collides with Ruby's collider/controller, deal Ruby damage
     private void OnTriggerStay2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
